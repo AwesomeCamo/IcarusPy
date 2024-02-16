348627f56a145cf1b3230903e297b4f1bd0a302d
Marco Schmitz <camo2708@gmail.com>
2024-02-16 14:32:19 +0100

    add car picture retrieval

--- car_list.py
+++ car_list.py
@@ -1,4 +1,5 @@
 import json
+import webbrowser
 
 from db_entries import insert_car_data
 
@@ -8,7 +9,7 @@ def update_car_list(session):
     search_url = "https://members-ng.iracing.com/data/car/get"
     car_url = json.loads(session.get(url=search_url).text)["link"]
     list_of_car_dicts = json.loads(session.get(url=car_url).text)
-    # print(list_of_car_dicts)
+    car_id_list = []
     for car_dict in list_of_car_dicts:
         car_info = car_dict["search_filters"]
         if "dirt" not in car_info and "oval" not in car_info or \
@@ -19,5 +20,16 @@ def update_car_list(session):
                 else:
                     discipline = "Formula"
                 insert_car_data(car_dict, discipline)
-
+                car_id_list.append(car_dict["car_id"])
+    #get_car_pictures(session, car_id_list)
     print("Car list update complete")
+
+
+def get_car_pictures(session, car_id_list):
+    # retrieves the link to every car in Icarus Career Mode and opens the webpage to its preview picture
+    search_url = "https://members-ng.iracing.com/data/car/assets"
+    asset_url = json.loads(session.get(url=search_url).text)["link"]
+    asset_dict = json.loads(session.get(url=asset_url).text)
+    for i in car_id_list:
+        webbrowser.open("https://images-static.iracing.com%s/%s" % (asset_dict[str(i)]["folder"],
+                                                                    asset_dict[str(i)]["small_image"]))
